ea31ceb57221af4c6226639429d11e53534730c5
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-02-23 13:51:19 -0700

    Fixed Jump Ledge Bug

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -86,7 +86,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 1f, groundLayer);
     }
 
     private bool IsLeftWalled()
