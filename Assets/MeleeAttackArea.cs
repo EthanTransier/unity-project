df6946f511ffdd190b5d3451b3c58d41205a5c43
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-02-27 14:54:52 -0700

    Update bot movement

--- Assets/MeleeAttackArea.cs
+++ Assets/MeleeAttackArea.cs
@@ -6,9 +6,11 @@ public class MeleeAttackArea : MonoBehaviour
 {
     private int damage = 40;
 
-    private void OnTriggerEnter2D(Collider2D collider){
-        if(collider.GetComponent<Health>() != null){
-            health.Damage(damage);
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.GetComponent<Health>() != null)
+        {
+            // health.Damage(damage);
         }
     }
 }
