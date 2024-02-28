378cf2ad6c75416e3bb934b2a75f186e9365b248
EthanTransier <110576660+EthanTransier@users.noreply.github.com>
2024-02-28 14:44:43 -0700

    Knockback update

--- Assets/MeleeAttackArea.cs
+++ Assets/MeleeAttackArea.cs
@@ -55,8 +55,9 @@ public class MeleeAttackArea : MonoBehaviour
         rend.material.color = flashColor;
         enemyObject.GetComponent<BotMovement>().ToggleMovement(false); // Disable movement during knockback
         // yield return new WaitForSeconds(flashDuration);
+         yield return new WaitForSeconds(knockbackDuration);
         rend.material.color = originalColor;
-        yield return new WaitForSeconds(knockbackDuration); // Adjust knockback duration as needed
+        // Adjust knockback duration as needed
         enemyObject.GetComponent<BotMovement>().ToggleMovement(true); // Re-enable movement after knockback
     }
 
